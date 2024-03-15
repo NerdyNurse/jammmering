ce0321fd3e5f28d61928c85555697d13f6e7b550
Amalie Baert <amalie@nerdynurse.dk>
2024-03-15 22:57:49 +0100

    first commit initial static components

---
+++ src/components/PlayList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Playlist(){
+
+    return (
+        <div>
+            <h2>Play list</h2>
+            <button>Save to Spotify</button>
+        </div>
+    )
+}
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SearchBar() {
+
+    return(
+        <div>
+            <h1>Search Bar</h1>
+            <input type='text' />
+            <button>search</button>
+        </div>
+    )
+}
